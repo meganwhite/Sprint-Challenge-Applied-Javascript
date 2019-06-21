8c63ab6812bf57bca8a5fc5dfc14b3f3bcd6a34d
meganwhite <mfw247@gmail.com>
2019-06-21 14:58:58 -0400

    started building carousel class

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,12 +1,17 @@
-// class Carousel {
-//     constructor(carouselImg){
-//         this.carouselImg = carouselImg;
-        
-//         this.carouselImg.addEventListener('click',this.selectTab.bind(this));
+class Carousel {
+    constructor(carousel){
+        this.carousel = carousel;
+        let next = carousel.querySelector('.right-button');
+        let prev = carousel.querySelector('.left-button');
+        let imgs = carousel.querySelectorAll('.carousel img');
+        let counter = 0;
+        this.next.addEventListener('click',this.slideRight.bind(this));
+        this.prev.addEventListener('click',this.slideLeft.bind(this));
+    }
 
-// }
+}
 
-// let carouselImgs = document.querySelectorAll('.carousel img').forEach(img => new Carousel(img));
+let carousel = document.querySelectorAll('.carousel').forEach(carousel => new Carousel(carousel));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
